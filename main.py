a9bc722fccea78759782de42a0dd9d147d07a0b3
Ana Paredes <ana@LAPTOP-7KDFK4JQ.localdomain>
2021-11-05 19:32:16 -0400

    [ADD] isEven and isPrime functions

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+
 def fib(n):
     if n > 1:
         return fib(n-1) + fib(n-2)
     return n
-    
+
+def isPrime(num):
+  if num < 1:
+        return False
+  elif num == 2:
+        return True
+  else:
+        for i in range(2, num):
+            if num % i == 0:
+              return False
+  return True 
+
+def isEven(num):
+  if num%2==0:
+     return True
+  else:
+    return False
+
+
 for i in range(50):
-    print(fib(i))
+  print(fib(i), isPrime(i), isEven(i))
