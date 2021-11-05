8802b87896e2b52f3f51ef21a1d5e2abfdf21360
Ana Paredes <ana@LAPTOP-7KDFK4JQ.localdomain>
2021-11-05 19:44:11 -0400

    [FIX] some errors

--- main.py
+++ main.py
@@ -6,23 +6,24 @@ def fib(n):
 
 def isPrime(num):
   if num < 1:
-        return False
+     return False
   elif num == 2:
         return True
   else:
         for i in range(2, num):
             if num % i == 0:
-              return False
-  return True 
+             return False
+  return True
 
 def isEven(num):
   if num%2==0:
      return True
   else:
     return False
+
 def digits(num):
   x = [int(a) for a in str(num)]
   print(x)
 
 for i in range(50):
-  print(fib(i), isPrime(i), isEven(i), digits(i))
+  print(fib(i), isPrime(i), isEven(i), digits(i)) 
